5e74c3cc37cdb6615de50e0dcf1d547cb4b91bf6
Muna Mashhour <munamashhour@gmail.com>
2019-10-08 21:47:48 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,39 @@
-# Add your code here
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end     
+
+    def self.print_all
+       puts @@all.map{ |dog| dog.name }
+    end 
+
+    def save
+        @@all << self
+    end 
+
+end
+
+pluto = Dog.new("Pluto") 
+fido = Dog.new("Fido") 
+maddy = Dog.new("Maddy") 
+
+Dog.all
+
+Dog.clear_all
+
+Dog.print_all
+
