41d13f6b1396337fc8aa8f5bf263d19090a27ee0
Hope <hopegiometti@gmail.com>
2019-10-09 14:20:30 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+ require 'pry'
+ 
+ class Dog
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        # @@all << self 
+        save  
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+ end
+
+
+ fido = Dog.new("fido")
