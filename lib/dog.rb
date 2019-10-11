a2e7f4ef9f59535e741ae42cafbbaeaf7039526d
Vannida Lim <vannida.lim@gmail.com>
2019-10-11 18:13:16 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+require 'pry'
+
+class Dog
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+       @@all.map do |instance|
+            puts instance.name
+       end 
+    end 
+
+    def save
+        @@all << self
+    end
+
+    def name
+        @name
+    end
+end 
